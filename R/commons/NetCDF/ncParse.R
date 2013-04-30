a57689ea556c7239eae26b395d0027b34640c403
xhoenner <xavier.hoenner@utas.edu.au>
2013-04-30 13:06:01 +1000

    Update ncParse.R
    Resolved issue related to the 'Convert time values into dates' section

--- R/commons/NetCDF/ncParse.R
+++ R/commons/NetCDF/ncParse.R
@@ -167,9 +167,9 @@ for ( v in 1:length( dimvarnames)){
 	}}} else { natts <- 0}
 
 	##### Convert time values into dates
-	if ( dimvarnames[v] == "TIME" | dimvarnames[v] == "JULD" | dimvarnames[v] == "JULD_LOCATION" && length( grep( "days", ncatt_get( ncdf, dimvarnames[v], "units"))) == 1) unit <- 3600*24 else if ( dimvarnames[v] == "TIME" | dimvarnames[v] == "JULD" | dimvarnames[v] == "JULD_LOCATION" && length( grep( "hours", ncatt_get( ncdf, dimvarnames[v], "units"))) == 1) unit <- 3600 else unit <- 1
-	if ( dimvarnames[v] == "TIME" | dimvarnames[v] == "JULD" | dimvarnames[v] == "JULD_LOCATION" ) data <- as.POSIXlt( data*unit, origin="1950-01-01", tz="UTC")
-	
+	if ( ( dimvarnames[v] == "TIME" | dimvarnames[v] == "JULD" | dimvarnames[v] == "JULD_LOCATION") == TRUE && (length( grep( "days", ncatt_get( ncdf, dimvarnames[v], "units"))) == 1) == TRUE) unit <- 3600*24 else
+	if ( (dimvarnames[v] == "TIME" | dimvarnames[v] == "JULD" | dimvarnames[v] == "JULD_LOCATION") == TRUE && (length( grep( "hours", ncatt_get( ncdf, dimvarnames[v], "units"))) == 1) == TRUE) unit <- 3600 else unit <- 1
+	if ( dimvarnames[v] == "TIME" | dimvarnames[v] == "JULD" | dimvarnames[v] == "JULD_LOCATION" ) data <- as.POSIXlt( data*unit, origin="1950-01-01", tz="UTC")	
 	##### Extract data from dimensions and variables
 	if ( parserOption != "metadata" && parserOption != "all") stop( "parserOption value invalid")
 	if ( parserOption == "metadata" && dimvartype[v] == "Dimension") dataset$dimensions[[dimvarnames[v]]] <- list()
