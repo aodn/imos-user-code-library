f3c1b0ddef23b1c83408133efb44b754f23e0813
mhidas <marty.hidas@utas.edu.au>
2013-05-24 17:24:58 +1000

    a bit more simplifying, cleaning up and adding some comments

--- Python/demos/anmn_adcp.py
+++ Python/demos/anmn_adcp.py
@@ -11,44 +11,50 @@
 # The script is distributed under the terms of the GNU General Public License
 
 from netCDF4 import Dataset, num2date
-from numpy import ones, array, meshgrid
+from numpy import arange, meshgrid
 from matplotlib.pyplot import (figure, subplot, pcolor, clim, colorbar, title, 
                                xlabel, ylabel, plot, setp, show)
 import matplotlib.colors as mcolors
 from matplotlib.dates import MONTHLY, DateFormatter, rrulewrapper, RRuleLocator
 
+# open the dataset
 anmn_URL = 'http://thredds.aodn.org.au/thredds/dodsC/IMOS/eMII/demos/ANMN/WA/WATR50/Velocity/IMOS_ANMN-WA_VATPE_20120516T040000Z_WATR50_FV01_WATR50-1205-Workhorse-ADCP-498_END-20121204T021500Z_C-20121207T023956Z.nc' 
 anmn_DATA = Dataset(anmn_URL) 
 
+# create short-cuts to the variables we're interested in
 UCUR = anmn_DATA.variables['UCUR']
 UCURqc = anmn_DATA.variables['UCUR_quality_control']
-DEPTH = anmn_DATA.variables['HEIGHT_ABOVE_SENSOR']
+HEIGHT = anmn_DATA.variables['HEIGHT_ABOVE_SENSOR']
+TIME = anmn_DATA.variables['TIME']
 
 # extract time values and convert into an array of datetime objects
-TIME = anmn_DATA.variables['TIME']
 timeData = num2date(TIME[:], TIME.units, TIME.calendar)
 
-depthData = DEPTH[:]
+# extract height and eastward velocity values
+heightData = HEIGHT[:]
 uCurrentData = UCUR[:]
+# Note: because UCUR has some missing values, this gives us a numpy
+# MaskedArray object, which works the same way as a normal array,
+# except missing values are automatically excluded from calculations
+# and plots.
 
-#it is a lot more relevant for ADCP data to plot the good and probably good data only (flags 1 and 2).
+# it is a lot more relevant for ADCP data to plot the good and
+# probably good data only (flags 1 and 2).
 qcLevel = [1, 2]
 qcIndex = ( UCURqc[:] == qcLevel[0]) | ( UCURqc[:] == qcLevel[1] )
+# update the mask to exclude all but the good data data
+uCurrentData.mask = ~qcIndex
 
 # get the flag meaning values to add it later in the figure title
 flag_meanings = (UCURqc.flag_meanings).split()
 
-# we modify the mask 
-uCurrentData.mask = ~qcIndex
+# create a profile index variable because pcolor can't handle datetime
+# objects on the x axis
+profIndex = arange(len(timeData)
 
-# creation of a observation/profile variable  because pcolor can't handle a time object in the x axis
-sizer = ones((1,len(depthData)),'float') 
-profIndex = array(range(len(timeData)))
-profIndex = profIndex.reshape(len(timeData),1) 
-prof_2D =  profIndex * sizer
-   
-# we create a matrix of similar size to be used afterwards with pcolor  
-[depthData_mesh,prof_2D_mesh] = meshgrid(depthData,profIndex)
+# create a matrices of the coordinate variables (same shape as
+# uCurrentData) to be used with pcolor
+heightData_mesh, prof_2D_mesh = meshgrid(heightData, profIndex)
 
 
 # creation of a blue and red colormap centered in white
@@ -62,18 +68,19 @@ cmap = mcolors.LinearSegmentedColormap.from_list(name='red_white_blue',
                                                  N=len(levs)-1,
                                                  )
                                             
-# plot adcp 
+# plot current profiles
 figure1 =figure( figsize=(13, 18), dpi=80, facecolor='w', edgecolor='k')
 ax1 = subplot(211)
 
-pcolor(prof_2D_mesh , depthData_mesh , uCurrentData[:,:,0,0],cmap=cmap)
+pcolor(prof_2D_mesh , heightData_mesh , uCurrentData[:,:,0,0],cmap=cmap)
 clim(UCUR.valid_min, UCUR.valid_max)
 cbar = colorbar()
 cbar.ax.set_ylabel(UCUR.long_name + ' in ' + UCUR.units)
 
-title(anmn_DATA.title + '\nplot of ' + flag_meanings[qcLevel[0]] + ' and ' + flag_meanings[qcLevel[1]] + ' only')
+title(anmn_DATA.title + '\nplot of ' + flag_meanings[qcLevel[0]] + 
+      ' and ' + flag_meanings[qcLevel[1]] + ' only')
 xlabel('Profile Index')
-ylabel(DEPTH.long_name +' in ' + DEPTH.units)
+ylabel(HEIGHT.long_name +' in ' + HEIGHT.units)
 
 # plot profile index with time 
 ax2 = subplot(212)
@@ -81,10 +88,10 @@ plot(timeData,profIndex)
 ylabel('Profile Index')
 xlabel(anmn_DATA.variables['TIME'].long_name +' in DD/MM/YY')
 
+# format date ticks
 rule = rrulewrapper(MONTHLY, bymonthday=1, interval=1)
 formatter = DateFormatter('%d/%m/%y')
 loc = RRuleLocator(rule)
-
 ax2.xaxis.set_major_locator(loc)
 ax2.xaxis.set_major_formatter(formatter)
 labels = ax2.get_xticklabels()
