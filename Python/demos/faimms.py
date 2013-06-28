eb70aa5d9bd89b56de6f85d7fd26e8798a37b21f
mhidas <marty.hidas@utas.edu.au>
2013-06-28 16:58:50 +1000

    removed imosNetCDF and cleaned up imports

--- Python/demos/faimms.py
+++ Python/demos/faimms.py
@@ -10,17 +10,14 @@
 # Copyright 2013 IMOS
 # The script is distributed under the terms of the GNU General Public License
 
-from netCDF4 import Dataset
-from datetime import datetime, timedelta
-from pylab import * 
-import numpy
-import matplotlib.pyplot as plt    
-from imosNetCDF import *
+from numpy import where
+from netCDF4 import Dataset, num2date
+from matplotlib.pyplot import figure, subplot, plot, xlabel, ylabel, title, setp, show
+from matplotlib.dates import DAILY, DateFormatter, rrulewrapper, RRuleLocator
 
 ############# FAIMMS
 FAIMMS_URL = 'http://thredds.aodn.org.au/thredds/dodsC/IMOS/eMII/demos/FAIMMS/Myrmidon_Reef/Sensor_Float_1/water_temperature/sea_water_temperature@5.0m_channel_114/2012/QAQC/IMOS_FAIMMS_T_20121201T000000Z_FV01_END-20130101T000000Z_C-20130426T102459Z.nc' 
 faimms_DATA = Dataset(FAIMMS_URL) 
-metadata = getAttNC(faimms_DATA)
 
 qcLevel = 1 # only good data are being used
 
@@ -28,19 +25,18 @@ TEMP = faimms_DATA.variables['TEMP'][:]
 TEMP_qcFlag = faimms_DATA.variables['TEMP_quality_control']
 index_qcLevel = where( TEMP_qcFlag[:,0,0] == qcLevel)
 
-
-timeData = convertTime(faimms_DATA.variables['TIME'])[index_qcLevel]
+TIME = faimms_DATA.variables['TIME']
+timeData = num2date(TIME, TIME.units)[index_qcLevel]
 tempData = TEMP[index_qcLevel[0][:],0,0]
 
 figure1 = figure(num=None, figsize=(15, 10), dpi=80, facecolor='w', edgecolor='k')
 ax1 = subplot(111)
 plot (timeData,tempData[:,])
 
-title(metadata['title'] + '\n' + "%0.2f" % faimms_DATA.variables['TEMP'].sensor_depth + ' m depth' + '\nlocation:lat=' + "%0.2f" % faimms_DATA.variables['LATITUDE'][:] + '; lon=' + "%0.2f" % faimms_DATA.variables['LONGITUDE'][:] )
+title(faimms_DATA.title + '\n' + "%0.2f" % faimms_DATA.variables['TEMP'].sensor_depth + ' m depth' + '\nlocation:lat=' + "%0.2f" % faimms_DATA.variables['LATITUDE'][:] + '; lon=' + "%0.2f" % faimms_DATA.variables['LONGITUDE'][:] )
 xlabel( faimms_DATA.variables['TIME'].long_name)
 ylabel( faimms_DATA.variables['TEMP'].standard_name +' in ' + faimms_DATA.variables['TEMP'].units)
 
-from matplotlib.dates import DAILY, DateFormatter, rrulewrapper, RRuleLocator
 rule = rrulewrapper(DAILY,  interval=1)
 formatter = DateFormatter('%d/%m/%y')
 loc = RRuleLocator(rule)
@@ -49,4 +45,4 @@ ax1.xaxis.set_major_formatter(formatter)
 labels = ax1.get_xticklabels()
 setp(labels, rotation=30, fontsize=10)
 
-plt.show()
+show()
