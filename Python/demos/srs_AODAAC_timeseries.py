ed3b854ec8fe50e3a1241196979723c238001b99
lbesnard <laurent.besnard@utas.edu.au>
2014-08-27 18:23:17 +1000

    modification following Marty suggestions. It still seems like the exit() is not taken in acount at line 102 and it drives me nuts

--- Python/demos/srs_AODAAC_timeseries.py
+++ Python/demos/srs_AODAAC_timeseries.py
@@ -23,27 +23,25 @@
 # It asks for a longitude and latitude values, and return if there is any data available 
 # a plot, as well as a csv file
 #
-from netCDF4 import Dataset
-from numpy import meshgrid
-import numpy 
+#if __name__ == "__main__":
+import numpy
 from netCDF4 import Dataset, num2date
-from matplotlib.pyplot import figure, subplot, plot, xlabel, ylabel, title, setp, show
-from matplotlib.dates import DAILY, DateFormatter, rrulewrapper, RRuleLocator
-import matplotlib.pyplot as plt
-import sys
+from matplotlib.pyplot import figure,  plot, xlabel, ylabel, title, setp, show
+import matplotlib.pyplot
 import csv
-######## 
-
-# Box Dialog to select a L3P AODAAC file
 from Tkinter import Tk
 from tkFileDialog import askopenfilename
+
+#	try:
+	# Box Dialog to select an AODAAC file
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-srs_AODAAC_URL=askopenfilename(filetypes=[("Image Files","*.nc")])
+srs_AODAAC_URL=askopenfilename(filetypes=[("NetCDF file","*.nc")])
 srs_AODAAC_DATA = Dataset(srs_AODAAC_URL) 
 
 
 
-# Look for the Variable names in the NetCDF file
+# Look for the Variable names in the NetCDF file. 
+# There is CURRENTLY always ONLY 1 main Var in the AODAAC file
 for v in srs_AODAAC_DATA.variables: 
 	if v[0:3].lower() == 'LAT'.lower():
 		latName = v
@@ -55,13 +53,12 @@ for v in srs_AODAAC_DATA.variables:
 		mainVarName = v
 
 
-# loading the data
+# loading the data into arrays
 mainVar = srs_AODAAC_DATA.variables[mainVarName]
 lat =srs_AODAAC_DATA.variables[latName]
 lon = srs_AODAAC_DATA.variables[lonName]
 TIME = srs_AODAAC_DATA.variables[timeName]
-#timeData = zip(num2date(TIME[:], TIME.units))
-timeData = (num2date(TIME[:], TIME.units))
+timeData = num2date(TIME[:], TIME.units)
 
 # look for the bounding box values
 lat_min = lat[0]
@@ -71,7 +68,8 @@ lon_max = lon[-1]
 
 print ('Latitude and longitude values to get a timseries plot at the wanted location') 
 
-# not checking values types by the user are numbers. We assume the user is smart
+# Not checking if values entered by the user are numbers. We assume the user wants to make 
+# this code run, and not try to kill it
 
 # prompt Lon
 prompt_text_Lon = 'Enter longitude value between ' + str(min(lon)) + ' and '+ str(max(lon)) + ": "
@@ -90,61 +88,61 @@ while ((float(input_Lat) <  min(lat) ) or (float(input_Lat) >  max(lat) )):
 lon_index = int((float(input_Lon)-lon_min)*lon.size/(lon_max - lon_min ))-1
 lat_index = int((float(input_Lat)-lat_min)*lat.size/(lat_max - lat_min ))-1
 
+# load timeseries data at the requested location
 mainVar_values = mainVar[:,lat_index,lon_index]
 
-# this is a test to check if there the mainVar_values array is full of fillvalue or no, ie masked
+# this is a test to check if the mainVar_values array is full of fillvalue or no, ie masked
+nValues = 0 # just to initiate
+
 try:
-	mainVar_values.mask
-	try: 
-	    test = numpy.min(mainVar_values[~mainVar_values.mask])
-	    # Continue with my_min
-	except ValueError: 
-	    # Bail or what have you
-	    print >>  sys.stderr, 'There is no data at the selected point'
-	    sys.exit(1)
-
-	## save timeseries at point in CSV
-
-	timeData_unmasked = timeData[mainVar_values.mask == False]
-	mainVar_values_unmasked = mainVar_values[mainVar_values.mask == False]
-
-	nValues = mainVar_values[mainVar_values.mask == False].size
-
+	test_nGoodValues = mainVar_values[~mainVar_values.mask].size
+	if test_nGoodValues == 0:
+		# Bail
+		print  'There is no data at the selected point'
+		exit()
+	else:
+		timeData_unmasked = timeData[~mainVar_values.mask]
+		mainVar_values_unmasked = mainVar_values[~mainVar_values.mask]
+		nValues = mainVar_values[~mainVar_values.mask].size
 except:
 	timeData_unmasked = timeData
 	mainVar_values_unmasked = mainVar_values
-
 	nValues = mainVar_values.size
 
-
-csvFilename =  srs_AODAAC_URL[0:-3] + '_lat_' +str(input_Lat) + '_lon_' +str(input_Lon) + '.csv'
-with open(csvFilename,'w') as outputfile:
-	wrtr = csv.writer(outputfile, delimiter=',', quotechar='"')
-	a = (srs_AODAAC_DATA.variables[timeName].long_name,srs_AODAAC_DATA.variables[mainVarName].long_name +  ' in ' + srs_AODAAC_DATA.variables[mainVarName].units)
-	wrtr.writerow(a) 
-	for index in range(nValues) :
-		a = (timeData_unmasked[index].strftime('%Y-%m-%d %H:%M:%S'),mainVar_values_unmasked[index])
+if nValues != 0:
+	## save timeseries at point in CSV
+	csvFilename =  srs_AODAAC_URL[0:-3] + '_lat_' +str(input_Lat) + '_lon_' +str(input_Lon) + '.csv'
+	with open(csvFilename,'w') as outputfile:
+		wrtr = csv.writer(outputfile, delimiter=',', quotechar='"')
+		a = (TIME.long_name,mainVar.long_name +  ' in ' + mainVar.units)
 		wrtr.writerow(a) 
-print ('CSV ouput to ' + csvFilename) 
+		for index in range(nValues) :
+			a = (timeData_unmasked[index].strftime('%Y-%m-%d %H:%M:%S'),mainVar_values_unmasked[index])
+			wrtr.writerow(a) 
+	print ('CSV ouput to ' + csvFilename) 
 
 
-# Plot
-figure1 =  figure(num=None, figsize=(15, 10), dpi=80, facecolor='w', edgecolor='k')
-plot (timeData,mainVar_values)
+	# Plot of the timeseries
+	figure1 =  figure(num=None, figsize=(15, 10), dpi=80, facecolor='w', edgecolor='k')
+	plot (timeData,mainVar_values)
 
-try :
-	title(srs_AODAAC_DATA.title + '-' +  srs_AODAAC_DATA.DSD_entry_id)
-except :
-    pass
+	try :
+		title(srs_AODAAC_DATA.title + '-' +  srs_AODAAC_DATA.DSD_entry_id)
+	except :
+		pass
 
-try :
-	xlabel(srs_AODAAC_DATA.variables[timeName].long_name )
-except :
-    pass
+	try :
+		xlabel(TIME.long_name )
+	except :
+		pass
 
-try :
-	ylabel(srs_AODAAC_DATA.variables[mainVarName].long_name +  ' in ' + srs_AODAAC_DATA.variables[mainVarName].units)
-except :
-    pass
+	try :
+		ylabel(mainVar.long_name +  ' in ' + mainVar.units)
+	except :
+		pass
 
-show()
+	show()
+	exit()
+else:
+	print 'There is no data at the selected point'
+	exit()
